042c1a471d241248d4f8493811a432539592ecfe
Bernardo A Mira García <bamira1@misena.edu.co>
2024-08-20 07:26:41 -0500

    Initial commit

---
+++ src/javanotes/Operator.java
@@ -0,0 +1,23 @@
+package javanotes;
+
+public class Operator {
+
+
+    public static void main(String[] args) {
+
+        // Arimeticos (+,-,/,*.%)
+
+        int num1 = 50;
+
+        int num2 = 35;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+
+
+
+
+    }
+}
